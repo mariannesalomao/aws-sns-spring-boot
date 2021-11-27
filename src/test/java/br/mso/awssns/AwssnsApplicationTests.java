18856480bf901d6f37e5abb4c68af54025df41fa
mariannesalomao <mariannesalomaodeoliveira@hotmail.com>
2021-11-27 11:59:43 -0300

    Enviando código da aula de SNS AWS

---
+++ src/test/java/br/mso/awssns/AwssnsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mso.awssns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
